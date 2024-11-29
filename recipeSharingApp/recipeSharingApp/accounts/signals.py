0191f37064c19fdc2af8f91c4289f0bd50693fed
nikolay_dimitrov256 <raystlin7@gmail.com>
2024-11-29 19:29:17 +0200

    feat: Created signal for creating gallery on profile creation

--- recipeSharingApp/recipeSharingApp/accounts/signals.py
+++ recipeSharingApp/recipeSharingApp/accounts/signals.py
@@ -3,6 +3,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from recipeSharingApp.accounts.models import Profile
+from recipeSharingApp.pictures.models import Gallery
 
 UserModel = get_user_model()
 
@@ -11,3 +12,9 @@ UserModel = get_user_model()
 def create_profile(sender, instance: UserModel, created: bool, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=Profile)
+def create_gallery(sender, instance: Profile, created: bool, **kwargs):
+    if created and not hasattr(instance, 'gallery'):
+        Gallery.objects.create(profile=instance)
