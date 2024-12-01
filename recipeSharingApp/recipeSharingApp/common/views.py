6891f0b2b2880436bc77f4ea49624e9b5537feee
nikolay_dimitrov256 <raystlin7@gmail.com>
2024-12-02 00:33:52 +0200

    change: Changed the Home page to List View

--- recipeSharingApp/recipeSharingApp/common/views.py
+++ recipeSharingApp/recipeSharingApp/common/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView, TemplateView
+from django.views.generic import ListView
+
+from recipeSharingApp.recipes.models import Recipe
 
 
-class HomeView(TemplateView):
-    template_name = 'common/home-page.html'
+class HomeView(ListView):
+    model = Recipe
+    template_name = 'recipes/recipe-list.html'
+
+    def get_queryset(self):
+        return Recipe.objects.all().order_by('-created_at')
