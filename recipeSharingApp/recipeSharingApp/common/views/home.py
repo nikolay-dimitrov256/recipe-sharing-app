ac05376a715e40849eac1c2bdcf477603a9c7af8
nikolay_dimitrov256 <raystlin7@gmail.com>
2024-12-07 18:09:38 +0200

    chore: Deleted unused code

--- recipeSharingApp/recipeSharingApp/common/views/home.py
+++ recipeSharingApp/recipeSharingApp/common/views/home.py
@@ -17,12 +17,3 @@ class HomeView(SetIsLikedInContextMixin, ListView):
                     .order_by('-is_followed', '-created_at', '-updated_at')
                     )
         return Recipe.objects.all().order_by('-created_at', '-updated_at')
-
-    # def get_context_data(self, object_list=None, **kwargs):
-    #     context = super().get_context_data(object_list=None, **kwargs)
-    #     user = self.request.user
-    #
-    #     for recipe in context['object_list']:
-    #         recipe.is_liked = recipe.likes.filter(author=user).exists() if self.request.user.is_authenticated else False
-    #
-    #     return context
