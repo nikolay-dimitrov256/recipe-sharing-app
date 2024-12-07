7a6bf83ed10b361ba586147ead7d67f9106fc7ea
nikolay_dimitrov256 <raystlin7@gmail.com>
2024-12-07 17:15:41 +0200

    change: Changed home page to show recipes by followed first

--- recipeSharingApp/recipeSharingApp/common/views/home.py
+++ recipeSharingApp/recipeSharingApp/common/views/home.py
@@ -1,3 +1,4 @@
+from django.db.models import Exists, OuterRef
 from django.views.generic import ListView
 
 from recipeSharingApp.recipes.models import Recipe
@@ -5,9 +6,15 @@ from recipeSharingApp.recipes.models import Recipe
 
 class HomeView(ListView):
     model = Recipe
-    template_name = 'recipes/recipe-list.html'
+    template_name = 'common/home.html'
 
     def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return (Recipe.objects
+                    .annotate(
+                        is_followed=Exists(self.request.user.following.filter(pk=OuterRef('author_id'))))
+                    .order_by('-is_followed', '-created_at', '-updated_at')
+                    )
         return Recipe.objects.all().order_by('-created_at', '-updated_at')
 
     def get_context_data(self, object_list=None, **kwargs):
