6306604cda3c2875c666819d902a7c375aff4755
nikolay_dimitrov256 <raystlin7@gmail.com>
2024-12-08 17:10:58 +0200

    feat: Developed fuctionality for making pictures main

--- recipeSharingApp/recipeSharingApp/pictures/urls.py
+++ recipeSharingApp/recipeSharingApp/pictures/urls.py
@@ -4,5 +4,6 @@ from recipeSharingApp.pictures import views
 urlpatterns = [
     path('<int:pk>/', include([
         path('delete/', views.DeletePictureView.as_view(), name='delete-picture'),
+        path('make-main/', views.make_main_picture_view, name='make-main'),
     ]))
 ]
--- recipeSharingApp/recipeSharingApp/pictures/views.py
+++ recipeSharingApp/recipeSharingApp/pictures/views.py
@@ -13,30 +13,6 @@ from recipeSharingApp.pictures.models import Picture
 UserModel = get_user_model()
 
 
-@login_required
-def delete_profile_picture(request, pk):
-    picture = get_object_or_404(Picture, pk=pk)
-    user = picture.gallery.profile.user
-
-    if user != request.user:
-        return HttpResponseForbidden('You are not allowed to edit this profile')
-
-    if picture:
-        try:
-            public_id = picture.image.public_id
-            uploader.destroy(public_id)
-            picture.delete()
-            messages.success(request, 'Profile picture deleted successfully!')
-
-        except Exception as e:
-            messages.error(request, f'An error occurred: {e}')
-
-    else:
-        messages.error(request, message='No profile picture to delete.')
-
-    return redirect('profile-details', user.pk)
-
-
 class DeletePictureView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Picture
     template_name = 'pictures/delete-picture.html'
@@ -55,3 +31,21 @@ class DeletePictureView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         elif picture.gallery.profile:
             return picture.gallery.profile.user == self.request.user
         return False
+
+
+@login_required
+def make_main_picture_view(request, pk):
+    picture = get_object_or_404(Picture, pk=pk)
+
+    if picture.gallery.recipe:
+        if picture.gallery.recipe.author != request.user:
+            return HttpResponseForbidden
+    elif picture.gallery.profile:
+        if picture.gallery.profile.user != request.user:
+            return HttpResponseForbidden
+
+    if request.method == 'POST':
+        picture.is_main = True
+        picture.save()
+
+    return redirect(request.META.get('HTTP_REFERER'))
