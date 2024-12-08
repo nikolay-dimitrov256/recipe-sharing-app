caf94a26f6d681178a1b1e52a6be3a635ffb7ece
nikolay_dimitrov256 <raystlin7@gmail.com>
2024-12-08 21:52:41 +0200

    feat: Added admin settings for galleries

--- recipeSharingApp/recipeSharingApp/pictures/admin.py
+++ recipeSharingApp/recipeSharingApp/pictures/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from recipeSharingApp.pictures.models import Gallery
+
+
+@admin.register(Gallery)
+class GalleryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'profile__user__email', 'recipe',)
+    search_fields = ('profile__user__email', 'recipe__title',)
+
