17229bbb056ae28a2f7e182f858862f91c6281b6
nikolay-dimitrov256 <raystlin7@gmail.com>
2024-12-09 12:27:43 +0200

    chore: Admin fields, styles

--- recipeSharingApp/recipeSharingApp/recipes/admin.py
+++ recipeSharingApp/recipeSharingApp/recipes/admin.py
@@ -8,3 +8,5 @@ class RecipeAdmin(admin.ModelAdmin):
     list_display = ('title', 'author__email',)
     search_fields = ('title', 'description', 'instructions')
     list_filter = ('author__email',)
+    ordering = ('-created_at', '-updated_at',)
+    readonly_fields = ('created_at', 'updated_at')
