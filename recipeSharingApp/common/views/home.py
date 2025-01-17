ac4bf70dc20b4927b9e3e96d57007c97eaa824d5
nikolay-dimitrov256 <raystlin7@gmail.com>
2025-01-17 09:41:51 +0200

    fix: Home view now shows own recipies with the same priority as those from followed users

--- recipeSharingApp/common/views/home.py
+++ recipeSharingApp/common/views/home.py
@@ -1,4 +1,4 @@
-from django.db.models import Exists, OuterRef
+from django.db.models import Exists, OuterRef, Q, F
 from django.views.generic import ListView
 
 from recipeSharingApp.recipes.mixins import SetRecipeDataInContextMixin
@@ -14,7 +14,10 @@ class HomeView(SetRecipeDataInContextMixin, ListView):
         if self.request.user.is_authenticated:
             return (Recipe.objects
                     .annotate(
-                        is_followed=Exists(self.request.user.following.filter(pk=OuterRef('author_id'))))
+                        is_followed=Exists(self.request.user.following.filter(pk=OuterRef('author_id')))
+                        |
+                        Q(author=self.request.user)
+                    )
                     .order_by('-is_followed', '-created_at', '-updated_at')
                     )
         return Recipe.objects.all().order_by('-created_at', '-updated_at')
